3882d13a0559b566bdff3c97e644e84cd4d98ed9
devarshdandale12 <devarshdandale12@gmail.com>
2026-07-17 11:12:34 +0530

    Time: 0 ms (100%), Space: 48.3 MB (50.43%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -4,7 +4,7 @@ class Solution {
         int high = nums.length-1;
         while(low<=high)
         {
-            int mid = low + (high - low) / 2;
+            int mid =  (high + low) / 2;
             if(target == nums[mid])
                 return mid;
             else if(target < nums[mid])
